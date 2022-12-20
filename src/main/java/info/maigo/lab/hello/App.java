b2bfb51e2656e97e96d8a82d018b118bb9f86323
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2022-12-20 15:02:48 +0530

    app. java

--- src/main/java/info/maigo/lab/hello/App.java
+++ src/main/java/info/maigo/lab/hello/App.java
@@ -1,34 +1,32 @@
-package info.maigo.lab.hello;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-import org.json.*;
-import com.mashape.unirest.http.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class App {
-
-  public static void main(String[] args) throws Exception {
-    for (String name: args) {
-      System.out.println("name: " + name);
-      String jsonString = new App(name).getUserInfoJSON();
-      JSONObject json = new JSONObject(jsonString);
-      System.out.println(json.toString(2));
-    }
-  }
-
-  private final String name;
-
-  public App(String name) {
-    this.name = name;
-  }
-
-  public String getUserInfoURL() {
-    // Users | GitHub Developer Guide: https://developer.github.com/v3/users/
-    return "https://api.github.com/users/" + name;
-  }
-
-  public String getUserInfoJSON() throws Exception {
-    String url = getUserInfoURL();
-    HttpResponse<JsonNode> res = Unirest.get(url).asJson();
-    return res.getBody().toString();
-  }
+public class TestClass {
+     public static  void main(String[] args){
+    	  TestClass t=new TestClass();
+    	  t.run();
+     }
+     public void run(){
+    	 WebDriver driver=null;
+    	 try{
+             driver=new FirefoxDriver();
+             String baseUrl="https://www.fileconvoy.com/index.php"; //upload file to this website
+             driver.get(baseUrl);
+             WebElement elem= driver.findElement(By.id("upfile_0"));
+             elem.sendKeys("file:///Users/owner1/Desktop/Puppy.java"); //Path to the file to upload
+             WebElement term=driver.findElement(By.id("readTermsOfUse")); //Click the term of use
+             term.click();
+             driver.findElement(By.id("upload_button")).click(); //click the submit button
+    	 } catch(NoSuchElementException e){
+    		 
+    	 }finally{
+    		 //firefoxDriver.quit();
+    	 }
+     }
 }
-
